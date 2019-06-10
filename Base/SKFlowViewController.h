123d02ae3eb0ee02b9071fa6581264ea5dd9aae3
李书康 <li@sk.local>
2019-06-10 10:42:43 +0800

    ++++++

--- Base/SKFlowViewController.h
+++ Base/SKFlowViewController.h
@@ -1,4 +1,50 @@
 
+
+//UIView
+#define SCREEN_WIDTH                [[UIScreen mainScreen] bounds].size.width
+#define SCREEN_HEIGHT               [[UIScreen mainScreen] bounds].size.height
+#define SCREEN_BOUNDS               [UIScreen mainScreen].bounds
+#define STATUS_BAR_HEIGHT           (([[UIScreen mainScreen] bounds].size.height == 812.0f || [[UIScreen mainScreen] bounds].size.height == 896.0f) ? 44.0f : 20.0f)
+#define BOTTOM_BAR_HEIGHT           (([[UIScreen mainScreen] bounds].size.height == 812.0f || [[UIScreen mainScreen] bounds].size.height == 896.0f) ? 34.0f : 0.0f)
+
+#define NAVIGATIONBAR_HEIGHT        44.0f
+#define TABBAR_HEIGHT               49.0f
+#define STATUS_NAV_BAR_HEIGHT       (STATUS_BAR_HEIGHT + NAVIGATIONBAR_HEIGHT)
+
+#define VIEW_WIDTH(view)            view.frame.size.width
+#define VIEW_HEIGHT(view)           view.frame.size.height
+#define VIEW_X(view)                view.frame.origin.x
+#define VIEW_Y(view)                view.frame.origin.y
+#define VIEW_LEFT(view)             view.frame.origin.x
+#define VIEW_TOP(view)              view.frame.origin.y
+#define VIEW_BOTTOM(view)           CGRectGetMaxY(view.frame)
+#define VIEW_RIGHT(view)            CGRectGetMaxX(view.frame)
+
+
+//json、model和dic之间的转换
+#define JsonFromDic(dic) [[NSString alloc] initWithData:[NSJSONSerialization dataWithJSONObject:dic options:NSJSONWritingPrettyPrinted error:nil] encoding:NSUTF8StringEncoding]
+#define JsonFromModel(model) [model respondsToSelector:@selector(toDictionary)] ? [NetWork jsonFromDic:[model performSelector:@selector(toDictionary)] == nil ? [[NSDictionary alloc] init] : (NSDictionary *)[model performSelector:@selector(toDictionary)]] : @""
+#define DicFromJson(json) [NSJSONSerialization JSONObjectWithData:[json dataUsingEncoding:NSUTF8StringEncoding] options:NSJSONReadingMutableContainers error:nil]
+#define DicFromModel(model) [NSJSONSerialization JSONObjectWithData:[([model respondsToSelector:@selector(toDictionary)] ? [NetWork jsonFromDic:[model performSelector:@selector(toDictionary)] == nil ? [[NSDictionary alloc] init] : (NSDictionary *)[model performSelector:@selector(toDictionary)]] : @"") dataUsingEncoding:NSUTF8StringEncoding] options:NSJSONReadingMutableContainers error:nil]
+
+//仅适用于sdWebImage
+#define skImage(urlString, defaultName) sd_setImageWithURL:[NSURL URLWithString:urlString] placeholderImage:[UIImage imageNamed:defaultName]
+
+//color
+//#define ColorWithRGBHex(hex)    [UIColor colorWithRGBHex:hex]
+#define COLOR_A(HEX, ALPHA) [UIColor colorWithRGBHexString:HEX alpha:ALPHA]
+#define COLOR(HEX) COLOR_A(HEX, 1.0)
+#define COLOR_RANDOM [UIColor colorWithRed:arc4random_uniform(255)/255.0 green:arc4random_uniform(255)/255.0 blue:arc4random_uniform(255)/255.0 alpha:1.0f]
+
+//加载xib
+#define VIEW_BY_XIB(xibName) [[[NSBundle mainBundle] loadNibNamed:xibName owner:self options:nil] lastObject]
+#define VIEWS_BY_XIB(xibName) [[NSBundle mainBundle] loadNibNamed:xibName owner:self options:nil]
+#define VIEW_INDEX_BY_XIB(xibName, index) [[NSBundle mainBundle] loadNibNamed:xibName owner:self options:nil].count > index ? [[NSBundle mainBundle] loadNibNamed:xibName owner:self options:nil][index] : [[UIView alloc] init]
+#define VIEW_BY_FRAME(x, y, w, h) [[UIView alloc] initWithFrame:CGRectMake(x, y, w, h)]
+#define SPACE_H(height) [[UIView alloc] initWithFrame:CGRectMake(0, 0, 1, height)]
+#define SPACE_W(width) [[UIView alloc] initWithFrame:CGRectMake(0, 0, width, 1)]
+
+
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
@@ -127,48 +173,60 @@ NS_ASSUME_NONNULL_END
 
 @end
 
+#pragma mark - SKColumn
 
-//UIView
-#define SCREEN_WIDTH                [[UIScreen mainScreen] bounds].size.width
-#define SCREEN_HEIGHT               [[UIScreen mainScreen] bounds].size.height
-#define SCREEN_BOUNDS               [UIScreen mainScreen].bounds
-#define STATUS_BAR_HEIGHT           (([[UIScreen mainScreen] bounds].size.height == 812.0f || [[UIScreen mainScreen] bounds].size.height == 896.0f) ? 44.0f : 20.0f)
-#define BOTTOM_BAR_HEIGHT           (([[UIScreen mainScreen] bounds].size.height == 812.0f || [[UIScreen mainScreen] bounds].size.height == 896.0f) ? 34.0f : 0.0f)
+enum SKColumn_alignment {
+    sk_column_left,
+    sk_column_center,
+    sk_column_right
+};
 
-#define NAVIGATIONBAR_HEIGHT        44.0f
-#define TABBAR_HEIGHT               49.0f
-#define STATUS_NAV_BAR_HEIGHT       (STATUS_BAR_HEIGHT + NAVIGATIONBAR_HEIGHT)
+#import <UIKit/UIKit.h>
 
-#define VIEW_WIDTH(view)            view.frame.size.width
-#define VIEW_HEIGHT(view)           view.frame.size.height
-#define VIEW_X(view)                view.frame.origin.x
-#define VIEW_Y(view)                view.frame.origin.y
-#define VIEW_LEFT(view)             view.frame.origin.x
-#define VIEW_TOP(view)              view.frame.origin.y
-#define VIEW_BOTTOM(view)           CGRectGetMaxY(view.frame)
-#define VIEW_RIGHT(view)            CGRectGetMaxX(view.frame)
+@interface SKColumn : UIView
+
+//默认屏宽
+@property (nonatomic, assign) double sk_column_width;
+//默认左对齐
+@property (nonatomic, assign) enum SKColumn_alignment alignment;
+//默认为空
+@property (nonatomic, strong) NSArray<UIView *> * _Nullable sk_column_childs;
+
+- (instancetype _Nullable )init;
+
+@end
+
+#pragma mark - SKRow
+
+enum SKRow_alignment {
+    sk_row_top,
+    sk_row_center,
+    sk_row_bottom
+};
+
+enum SKRow_direction {
+    start,
+    end
+};
+
+#import <UIKit/UIKit.h>
+
+@interface SKRow : UIView
+
+//默认为屏宽
+@property (nonatomic, assign) double sk_row_width;
+//默认为最高的子viewh高度
+@property (nonatomic, assign) double sk_row_height;
+//默认上对齐
+@property (nonatomic, assign) enum SKRow_alignment alignment;
+//默认从左开始
+@property (nonatomic, assign) enum SKRow_direction direction;
+//默认为空
+@property (nonatomic, strong) NSArray<UIView *> * _Nullable sk_row_childs;
+
+- (instancetype _Nullable )init;
+
+@end
 
 
-//json、model和dic之间的转换
-#define JsonFromDic(dic) [[NSString alloc] initWithData:[NSJSONSerialization dataWithJSONObject:dic options:NSJSONWritingPrettyPrinted error:nil] encoding:NSUTF8StringEncoding]
-#define JsonFromModel(model) [model respondsToSelector:@selector(toDictionary)] ? [NetWork jsonFromDic:[model performSelector:@selector(toDictionary)] == nil ? [[NSDictionary alloc] init] : (NSDictionary *)[model performSelector:@selector(toDictionary)]] : @""
-#define DicFromJson(json) [NSJSONSerialization JSONObjectWithData:[json dataUsingEncoding:NSUTF8StringEncoding] options:NSJSONReadingMutableContainers error:nil]
-#define DicFromModel(model) [NSJSONSerialization JSONObjectWithData:[([model respondsToSelector:@selector(toDictionary)] ? [NetWork jsonFromDic:[model performSelector:@selector(toDictionary)] == nil ? [[NSDictionary alloc] init] : (NSDictionary *)[model performSelector:@selector(toDictionary)]] : @"") dataUsingEncoding:NSUTF8StringEncoding] options:NSJSONReadingMutableContainers error:nil]
-
-//仅适用于sdWebImage
-#define skImage(urlString, defaultName) sd_setImageWithURL:[NSURL URLWithString:urlString] placeholderImage:[UIImage imageNamed:defaultName]
-
-//color
-//#define ColorWithRGBHex(hex)    [UIColor colorWithRGBHex:hex]
-#define COLOR_A(HEX, ALPHA) [UIColor colorWithRGBHexString:HEX alpha:ALPHA]
-#define COLOR(HEX) COLOR_A(HEX, 1.0)
-#define COLOR_RANDOM [UIColor colorWithRed:arc4random_uniform(255)/255.0 green:arc4random_uniform(255)/255.0 blue:arc4random_uniform(255)/255.0 alpha:1.0f]
-
-//加载xib
-#define VIEW_BY_XIB(xibName) [[[NSBundle mainBundle] loadNibNamed:xibName owner:self options:nil] lastObject]
-#define VIEWS_BY_XIB(xibName) [[NSBundle mainBundle] loadNibNamed:xibName owner:self options:nil]
-#define VIEW_INDEX_BY_XIB(xibName, index) [[NSBundle mainBundle] loadNibNamed:xibName owner:self options:nil].count > index ? [[NSBundle mainBundle] loadNibNamed:xibName owner:self options:nil][index] : [[UIView alloc] init]
-#define VIEW_BY_FRAME(x, y, w, h) [[UIView alloc] initWithFrame:CGRectMake(x, y, w, h)]
-#define SPACE_H(height) [[UIView alloc] initWithFrame:CGRectMake(0, 0, 1, height)]
-#define SPACE_W(width) [[UIView alloc] initWithFrame:CGRectMake(0, 0, width, 1)]
 
--- Base/SKFlowViewController.m
+++ Base/SKFlowViewController.m
@@ -761,5 +761,170 @@ NSString *const HEX_CHAR_SET = @"abcdefABCDEF1234567890";
     return image;
 }
 
+@end
+
+#pragma mark - SKColumn
+
+@implementation SKColumn
+
+- (instancetype)init{
+    self = [super init];
+    if (self) {
+        self.backgroundColor = UIColor.clearColor;
+        self.clipsToBounds = YES;
+        self.alignment = sk_column_left;
+        self.sk_column_width = SCREEN_WIDTH;
+    }
+    return self;
+}
+
+- (void)setSk_column_width:(double)sk_column_width {
+    _sk_column_width = sk_column_width;
+    [self skColumnLayout];
+}
+
+- (void)setAlignment:(enum SKColumn_alignment)alignment {
+    _alignment = alignment;
+    [self skColumnLayout];
+}
+
+- (void)setSk_column_childs:(NSArray<UIView *> *)sk_column_childs {
+    [self.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
+    _sk_column_childs = sk_column_childs;
+    
+    for (UIView *child_view in self.sk_column_childs) {
+        [self addSubview:child_view];
+    }
+    
+    [self skColumnLayout];
+}
+
+- (void)skColumnLayout {
+    self.frame = CGRectMake(0, 0, self.sk_column_width, VIEW_HEIGHT(self));
+    
+    double childs_total_height = 0;
+    for (UIView *child_view in self.sk_column_childs) {
+        
+        switch (self.alignment) {
+            case sk_column_left:
+                child_view.frame = CGRectMake(0, childs_total_height, VIEW_WIDTH(child_view), VIEW_HEIGHT(child_view));
+                break;
+            case sk_column_right:
+                child_view.frame = CGRectMake(VIEW_WIDTH(self) - VIEW_WIDTH(child_view), childs_total_height, VIEW_WIDTH(child_view), VIEW_HEIGHT(child_view));
+                break;
+            default:
+                child_view.frame = CGRectMake((VIEW_WIDTH(self) - VIEW_WIDTH(child_view)) / 2.0, childs_total_height, VIEW_WIDTH(child_view), VIEW_HEIGHT(child_view));
+                break;
+        }
+        [self addSubview:child_view];
+        childs_total_height += VIEW_HEIGHT(child_view);
+    }
+    self.frame = CGRectMake(0, 0, VIEW_WIDTH(self), childs_total_height);
+}
 
 @end
+
+#pragma mark - SKRow
+
+@implementation SKRow
+
+- (instancetype)init{
+    self = [super init];
+    if (self) {
+        self.backgroundColor = UIColor.clearColor;
+        self.clipsToBounds = YES;
+        self.alignment = sk_row_top;
+        self.direction = start;
+        self.sk_row_width = SCREEN_WIDTH;
+        self.sk_row_height = 0;
+    }
+    return self;
+}
+
+- (void)setSk_row_width:(double)sk_row_width {
+    _sk_row_width = sk_row_width;
+    [self skRowLayout];
+}
+
+- (void)setSk_row_height:(double)sk_row_height {
+    _sk_row_height = sk_row_height;
+    [self skRowLayout];
+}
+
+- (void)setAlignment:(enum SKRow_alignment)alignment {
+    _alignment = alignment;
+    [self skRowLayout];
+}
+
+- (void)setDirection:(enum SKRow_direction)direction {
+    _direction = direction;
+    [self skRowLayout];
+}
+
+- (void)setSk_row_childs:(NSArray<UIView *> *)sk_row_childs {
+    _sk_row_childs = sk_row_childs;
+    
+    [self.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
+    
+    double max_height = 0;
+    for (UIView *child_view in sk_row_childs) {
+        if (VIEW_HEIGHT(child_view) > max_height) {
+            max_height = VIEW_HEIGHT(child_view);
+        }
+    }
+    
+    if (self.sk_row_height <= 0) {
+        _sk_row_height = max_height;
+    }
+    
+    for (UIView *child_view in sk_row_childs) {
+        [self addSubview:child_view];
+    }
+    self.frame = CGRectMake(0, 0, self.sk_row_width, self.sk_row_height);
+    [self skRowLayout];
+}
+
+- (void)skRowLayout {
+    self.frame = CGRectMake(0, 0, self.sk_row_width, self.sk_row_height);
+    
+    double leftWidth = self.sk_row_width;
+    for (UIView *child_view in self.sk_row_childs) {
+        leftWidth -= VIEW_WIDTH(child_view);
+    }
+    if (leftWidth < 0) {
+        leftWidth = 0;
+    }
+    
+    double childs_total_width = 0;
+    
+    for (UIView *child_view in self.sk_row_childs) {
+        switch (self.alignment) {
+            case sk_row_top:
+                if (_direction == start) {
+                    child_view.frame = CGRectMake(childs_total_width, 0, VIEW_WIDTH(child_view), VIEW_HEIGHT(child_view));
+                } else {
+                    child_view.frame = CGRectMake(leftWidth + childs_total_width, 0, VIEW_WIDTH(child_view), VIEW_HEIGHT(child_view));
+                }
+                break;
+            case sk_row_bottom:
+                if (_direction == start) {
+                    child_view.frame = CGRectMake(childs_total_width, _sk_row_height - VIEW_HEIGHT(child_view), VIEW_WIDTH(child_view), VIEW_HEIGHT(child_view));
+                } else {
+                    child_view.frame = CGRectMake(leftWidth + childs_total_width, _sk_row_height - VIEW_HEIGHT(child_view), VIEW_WIDTH(child_view), VIEW_HEIGHT(child_view));
+                }
+                break;
+            default:
+                if (_direction == start) {
+                    child_view.frame = CGRectMake(childs_total_width, (_sk_row_height - VIEW_HEIGHT(child_view)) / 2.0, VIEW_WIDTH(child_view), VIEW_HEIGHT(child_view));
+                } else {
+                    child_view.frame = CGRectMake(leftWidth + childs_total_width, (_sk_row_height - VIEW_HEIGHT(child_view)) / 2.0, VIEW_WIDTH(child_view), VIEW_HEIGHT(child_view));
+                }
+                break;
+        }
+        childs_total_width += VIEW_WIDTH(child_view);
+    }
+}
+
+@end
+
+
